6af87d9e309eb49015277560677c98b748e9b708
Ekaterina Mitiashkina <algrince@gmail.com>
2022-08-16 14:53:33 +0200

    Prime game added

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+from brain_games.engine import start_game
+from brain_games.logic import prime
+
+
+def main():
+    start_game(prime)
+
+
+if __name__ == '__main__':
+    main()
