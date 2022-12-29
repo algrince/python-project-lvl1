e38ec3c77daa76e2f852be277014a615099e2383
Ekaterina Mitiashkina <algrince@gmail.com>
2022-12-29 16:59:02 +0100

    fix errors

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -12,11 +12,13 @@ MAX_NUM2 = 100
 
 def find_gcd(num1, num2):
     '''Finds the greater common divisor of two passed numbers'''
-    if num1 >= num2:
-        num1 = num1 % num2
-    elif num2 > num1:
-        num2 = num2 % num1
-    return find_gcd(num1, num2)
+    if num1 != 0 and num2 != 0:
+        if num1 >= num2:
+            num1 = num1 % num2
+        elif num2 > num1:
+            num2 = num2 % num1
+        return find_gcd(num1, num2)
+    return num1 + num2
 
 
 def generate_round(
